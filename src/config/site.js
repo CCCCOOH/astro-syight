5f20b03751926861bc79f0272cc692c607fbe788
CCCCOOH <c3156298376@163.com>
2025-09-02 06:45:21 +0800

    footer

--- src/config/site.js
+++ src/config/site.js
@@ -18,18 +18,23 @@ export const footer = [
   {
     name: 'bilibili',
     icon: '',
-    link: '/',
+    link: 'https://space.bilibili.com/450034908?spm_id_from=333.1007.0.0',
     color: '#f8309d'
   },
   {
     name: 'CSDN',
-    icon: '',
-    link: '/',
+    link: 'https://blog.csdn.net/2301_79640368?spm=1000.2115.3001.5343',
     color: '#ff5900'
   },
   {
-    name: 'Copyright © Astro Theme Syight',
+    // 请尊重作者
+    name: 'Copyright © Astro Syight',
     link: '/',
     color: 'black'
+  },
+  {
+    name: 'Link',
+    link: 'https://sygmail.github.io/',
+    color: 'red'
   }
 ]
